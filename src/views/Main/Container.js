43435c0a5173991f36ac6001dca67db197e796a8
Tomasz Wsuł <2nickers@gmail.com>
2016-06-01 23:24:38 +0200

    added <Header /> & base styling

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 import {searchNearby} from 'utils/googleApiHelpers'
+import Header from 'components/Header/Header'
 
+import styles from './styles.module.css'
 
 export class Container extends React.Component {
   constructor(props) {
@@ -35,13 +37,18 @@ export class Container extends React.Component {
       <div>
         Hello from the container
         <Map
+          className={styles.wrapper}
           google={this.props.google}
           onReady={this.onReady.bind(this)}
           visible={false}>
 
+          <Header />
+
+          <div className={styles.content}>
           {this.state.places.map(place => {
               return (<div key={place.id}>{place.name}</div>)
           })}
+          </div>
           </Map>
       </div>
     )
