458cd7b522cdb4af439947a3ae39677ebcee937e
Tomasz Wsuł <2nickers@gmail.com>
2016-06-02 00:26:45 +0200

    Listing&rating

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 import {searchNearby} from 'utils/googleApiHelpers'
+
 import Header from 'components/Header/Header'
+import Sidebar from 'components/Sidebar/Sidebar'
 
 import styles from './styles.module.css'
 
@@ -35,7 +37,6 @@ export class Container extends React.Component {
   render() {
     return (
       <div>
-        Hello from the container
         <Map
           className={styles.wrapper}
           google={this.props.google}
@@ -43,6 +44,10 @@ export class Container extends React.Component {
           visible={false}>
 
           <Header />
+          <Sidebar 
+              title={'Restaurants'}
+              places={this.state.places}
+          />
 
           <div className={styles.content}>
           {this.state.places.map(place => {
